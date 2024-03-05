3780e1b12df15cb01d52d71038cf5dff70a7b2c2
richardli166 <water.richard.li@gmail.com>
2024-03-05 09:22:50 -0500

    trying outtt

---
+++ vue-project2/src/views/BarGraph.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+const data = {
+    type: 'bar',
+    data:   {
+        datasets: [{
+            data: [{x: 10, y: 'A'}, {x: 15, y: 'B'}, {x: 5, y: 'C'}]
+}]
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
